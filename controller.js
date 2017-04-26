1ce628893cdcb12d334647c1f3879407aa03fbfd
loren pabst <loren pabst>
2017-04-25 20:21:25 -0600

    learning to use http request

--- controller.js
+++ controller.js
@@ -1,10 +1,16 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers().then(function(response){
+      $scope.users = response.data.data;
+    })
   }
 
-
   $scope.getUsers();
+
+
+
+
+  
 
 });
